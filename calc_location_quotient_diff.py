aa3acd0a2e256eea16d8c562a1915fdd23ff0c18
Chris Henrick <chrishenrick@gmail.com>
2018-06-03 18:37:46 -0700

    update lq diff script
    include lq values for 2002, 2015, and change in lq.

--- calc_location_quotient_diff.py
+++ calc_location_quotient_diff.py
@@ -1,3 +1,5 @@
+# calculate the change in location quotients between 2002 and 2015
+
 import pandas as pd
 import geopandas as gpd
 import numpy as np
@@ -13,6 +15,28 @@ df['serv_c'] = df['serv_lq'] - lq_2002['serv_lq']
 df['prof_c'] = df['prof_lq'] - lq_2002['prof_lq']
 df['supp_c'] = df['supp_lq'] - lq_2002['supp_lq']
 
-df = df[['make_c', 'serv_c', 'prof_c', 'supp_c', 'geometry', 'TRACTCE']]
+df['make_15'] = df['make_lq']
+df['serv_15'] = df['serv_lq']
+df['prof_15'] = df['prof_lq']
+df['supp_15'] = df['supp_lq']
 
-df.to_file('/Users/chrishenrick/fun/aemp_jobs_viz/data/tmp/lq_change_2015_2012')
+df['make_02'] = lq_2002['make_lq']
+df['serv_02'] = lq_2002['serv_lq']
+df['prof_02'] = lq_2002['prof_lq']
+df['supp_02'] = lq_2002['supp_lq']
+
+# remove old column names
+del df['make_lq']
+del df['serv_lq']
+del df['prof_lq']
+del df['supp_lq']
+
+# calculate area using CA State Plane (ft)
+df = df.to_crs(epsg=2227)
+df['area_sqm'] = (df.area * 0.09290304)
+
+# df = df[['make_c', 'serv_c', 'prof_c', 'supp_c', 'geometry', 'TRACTCE']]
+
+df.to_file('/Users/chrishenrick/fun/aemp_jobs_viz/data/tmp/lq_2015_2012')
+
+# TODO: SF Bay Area change
