55294f302e33fd9a63d463029ce8c7eb0381c9fe
Chris Henrick <chrishenrick@gmail.com>
2018-08-21 11:08:13 -0700

    updated & cleaned up yearly totals py to use cx walk instead of blocks.shp

--- calc_yearly_totals.py
+++ calc_yearly_totals.py
@@ -3,19 +3,19 @@
 import pandas as pd
 import geopandas as gpd
 import numpy as np
-import sys
+import sys, os
 from math import log10, floor
+from process_wac_data import filter_cx_walk, read_csv, write_csv
 
-def calc_year_totals(wac, blocks, year):
+def calc_year_totals(wac, cxwalk, year):
     """
     takes the workplace area characteristics dataframe and the 2010 census blocks dataframe
     aggregates wac categories into 4 super categories
     joins to blocks for 9 county SF bay area
     groups by tract id
     """
-    # rename geo id column
-    wac["geoid"] = wac["w_geocode"]
-    del wac["w_geocode"]
+    # join wac df to cxwalk df using fields w_geocode and tabblk2010
+    wac = wac.merge(cxwalk, how="inner", left_on="w_geocode", right_on="tabblk2010")
 
     # create new aggregate columns for various job sectors
     wac['makers'] = wac['CNS01'] + wac['CNS02'] + wac['CNS03'] + wac['CNS04'] + wac['CNS05'] + wac['CNS06'] + wac['CNS08']
@@ -26,49 +26,45 @@ def calc_year_totals(wac, blocks, year):
     # make sure they all add up
     assert sum(wac['C000'] -(wac['makers'] + wac['services'] + wac['professions'] + wac['support'])) == 0
 
-    # make a new data frame with only the columns we need from the wac dataframe
-    df = wac[['geoid', 'makers', 'services', 'professions', 'support']]
+    # keep only the columns we need from the wac dataframe
+    wac = wac[['trct', 'makers', 'services', 'professions', 'support']]
 
-    # create a new "geoid" column for the blocks shapes, dropping the preceeding zero
-    blocks["geoid"] = blocks.GEOID10.str[1:]
-
-    # cast the blocks geoid data type to numeric so it can be joined with the df
-    blocks[['geoid']] = blocks[['geoid']].apply(pd.to_numeric)
-
-    # merge (join) our blocks to the processed wac data
-    # `left_on` is the column for the blocks shapefile, `right_on` is the column for the tabular data
-    # `on` is when both columns share the same name
-    # `how` is the type of join, similar to SQL joins
-    blocks = blocks.merge(df, how="inner", on="geoid")
-
-    # only grab columns we need
-    filtered = blocks[['TRACTCE10', 'makers', 'services', 'professions', 'support']]
+    # group and aggregate data by census tract
+    wac = wac.groupby('trct', as_index=False).agg(np.sum)
 
     # store totals for each category, these will be the total jobs by category for the entire bay area
-    makers_total = filtered['makers'].sum()
-    services_total = filtered['services'].sum()
-    professions_total = filtered['professions'].sum()
-    support_total = filtered['support'].sum()
+    makers_total = wac['makers'].sum()
+    services_total = wac['services'].sum()
+    professions_total = wac['professions'].sum()
+    support_total = wac['support'].sum()
     all_total = makers_total + services_total + professions_total + support_total * 1.0
 
     return [year, makers_total, services_total, professions_total, support_total, all_total]
 
 def main():
-    blocks = gpd.read_file("/Users/chrishenrick/fun/aemp_jobs_viz/data/block_shp/census_blocks_2010_bay_area_4269.shp")
+    dirname = os.path.dirname(os.path.realpath("__file__"))
+
+    cxwalk = read_csv(os.path.join(dirname, "data/wac/ca_xwalk.csv.gz"), "ISO-8859-1")
+    cxwalk = filter_cx_walk(cxwalk)
+
+    # headers for output csv / dataframe
     columns = ["year", "makers", "services", "professions", "support", "all"]
-    outfilename = "/Users/chrishenrick/fun/aemp_jobs_viz/data/tmp/wac_yearly_breakdown.csv"
     df = pd.DataFrame(columns=columns)
 
+    # iterate over each wac data file and compute summary stats as new rows to the output df
     for n in range(2002, 2016):
-        print "current year is %s" % (n)
-        filename = "/Users/chrishenrick/fun/aemp_jobs_viz/data/wac/ca_wac_S000_JT00_%s.csv.gz" % (n)
+        print("current year is %s" % (n))
+        filename = os.path.join(dirname, "data/wac/ca_wac_S000_JT00_%s.csv.gz" % (n))
         wac = pd.read_csv(filename, compression="gzip", header=0, sep=',', quotechar='"')
-        df.loc[n - 2001] = calc_year_totals(wac, blocks, n)
+        df.loc[n - 2001] = calc_year_totals(wac, cxwalk, n)
 
     df[columns] = df[columns].apply(np.int64)
-
     print(df)
-    df.to_csv(outfilename, encoding='utf-8', index=False)
+
+    outfilename = os.path.join(dirname, "data/processed/wac_yearly_breakdown.csv")
+    write_csv(df, outfilename)
+
+    print("done!")
     sys.exit(0)
 
 
